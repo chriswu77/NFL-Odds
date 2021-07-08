b86fce7abce8191943e2e6bcbd19b1d946871da9
Chris Wu <cwu0569@gmail.com>
2021-07-07 17:20:17 -0700

    setup mongoDB and schema/model

---
+++ database/models/Game.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+  id: String,
+  sport_key: String,
+  commence_time: Date,
+  home_team: String,
+  away_team: String,
+  bookmakers: [{
+    key: String,
+    title: String,
+    last_update: Date,
+    markets: [{
+      key: String,
+      outcomes: [{
+        name: String,
+        price: Number,
+        point: Number
+      }]
+    }]
+  }]
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = Game;
