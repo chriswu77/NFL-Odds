9fa7d1852082740a339c08fb4538a8a26e83793f
Chris Wu <cwu0569@gmail.com>
2021-07-09 09:26:55 -0700

    created Slate component for individual slates

---
+++ client/src/components/Slate.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import parseISO from 'date-fns/parseISO';
+import Game from './Game';
+
+const Slate = (props) => {
+  const { name, games } = props;
+
+  const dayMap = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+  };
+
+  const monthMap = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
+  }
+
+  const gameDate = parseISO(games[0].commence_time);
+  const calendarDay = gameDate.getDate();
+  const day = gameDate.getDay();
+  const month = gameDate.getMonth();
+
+  let slateText;
+
+  if (name === 'thurs' || name === 'sat') {
+    slateText = `${dayMap[day]} - ${monthMap[month]} ${calendarDay}`;
+  } else if (name === 'sunMorning') {
+    slateText = `${dayMap[day]} Morning - ${monthMap[month]} ${calendarDay}`;
+  } else if (name === 'sunAfternoon') {
+    slateText = `${dayMap[day]} 1pm - ${monthMap[month]} ${calendarDay}`;
+  } else if (name === 'sunEvening') {
+    slateText = `${dayMap[day]} 4pm - ${monthMap[month]} ${calendarDay}`;
+  } else if (name === 'primetime') {
+    const game2Date = parseISO(games[1].commence_time);
+    const calendarDay2 = game2Date.getDate();
+    const month2 = game2Date.getMonth();
+
+    if (gameDate < game2Date) {
+      slateText = `Primetime - ${monthMap[month]} ${calendarDay} to ${monthMap[month2]} ${calendarDay2}`
+    } else {
+      slateText = `Primetime - ${monthMap[month2]} ${calendarDay2} to ${monthMap[month]} ${calendarDay}`
+    }
+  }
+
+  return (
+    <Row>
+      <Col>
+        <Row className="mx-auto mt-3 mb-3">
+          <Col className="pl-0">
+            <h2>{slateText}</h2>
+          </Col>
+        </Row>
+        {games.map((game) => <Game game={game} slate={name} key={game.id} />)}
+      </Col>
+    </Row>
+  );
+};
+
+export default Slate;
