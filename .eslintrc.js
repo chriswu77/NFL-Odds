ad82cc3a1120b77d83f0061d100f056706bdb66d
Chris Wu <cwu0569@gmail.com>
2021-07-19 19:04:45 -0700

    fixed eslint to work with prettier. was missing a plugin and had to add prettier as a rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,15 +11,14 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
-    "no-restricted-syntax": 'off',
-    "no-console": 'warn',
-    "no-plusplus": 'off',
-    "no-unused-vars": 'warn',
-    "no-continue": 'off',
-    "react/prop-types": 'off'
+    'prettier/prettier': 'error',
+    'no-restricted-syntax': 'off',
+    'no-console': 'warn',
+    'no-plusplus': 'off',
+    'no-unused-vars': 'warn',
+    'no-continue': 'off',
+    'react/prop-types': 'off',
   },
 };
