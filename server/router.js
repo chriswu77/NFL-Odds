15e48dc1d499df8c975a7cf8ed128f1592bf0fec
Chris Wu <cwu0569@gmail.com>
2021-07-19 19:09:14 -0700

    removed getAll route and updated controller function

--- server/router.js
+++ server/router.js
@@ -3,10 +3,8 @@ const controller = require('./controller');
 
 router.get('/refresh', controller.refresh);
 
-router.get('/all', controller.getAll);
-
 router.get('/current', controller.getCurrentWeek);
 
-router.get('/summary', controller.getSummary);
+router.get('/summary', controller.getGames);
 
 module.exports = router;
