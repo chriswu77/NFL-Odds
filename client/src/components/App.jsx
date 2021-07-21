74ff13d35802c69d17c4ba709707f49ecd24a62e
Chris Wu <cwu0569@gmail.com>
2021-07-20 18:13:29 -0700

    added button and functionality to toggle between current and next week. cleaned up some code

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
@@ -6,10 +6,16 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faLongArrowAltRight,
+  faLongArrowAltLeft,
+} from '@fortawesome/free-solid-svg-icons';
 import Slates from './Slates';
 
 const App = () => {
   const [week, setWeek] = useState(1);
+  const [isNextWeek, setIsNextWeek] = useState(false);
   const [slateData, setSlateData] = useState();
   const [requestsRemaining, setRequestsRemaining] = useState(
     localStorage.getItem('requestsRemaining') || 500
@@ -28,9 +34,13 @@ const App = () => {
     }
   };
 
+  useEffect(async () => {
+    await getCurrentWeek();
+  }, []);
+
   const getSummaryData = async () => {
     try {
-      const res = await axios.get('/api/games');
+      const res = await axios.get(`/api/games/${week}`);
       setSlateData(res.data);
     } catch (err) {
       console.log(err);
@@ -38,9 +48,8 @@ const App = () => {
   };
 
   useEffect(async () => {
-    await getCurrentWeek();
     await getSummaryData();
-  }, []);
+  }, [week]);
 
   const refreshData = async () => {
     try {
@@ -54,7 +63,6 @@ const App = () => {
       localStorage.setItem('requestsUsed', res.data.used);
 
       await getCurrentWeek();
-      await getSummaryData();
 
       setIsLoading(false);
     } catch (err) {
@@ -73,17 +81,54 @@ const App = () => {
     </div>
   );
 
+  const toggleWeek = () => {
+    setIsNextWeek(!isNextWeek);
+  };
+
+  const isFirstRun = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      return;
+    }
+
+    if (isNextWeek) {
+      setWeek(week + 1);
+    } else {
+      setWeek(week - 1);
+    }
+  }, [isNextWeek]);
+
   return (
     <>
       <Container fluid className="main-container">
         <Row className="mx-auto mb-5">
           <Col className="pl-0">
             <h1 className="display-6 ">NFL 2021 - WEEK {week} ODDS</h1>
-            <Button variant="primary" onClick={refreshData}>> WEEK 2</Button>
+            <Button variant="primary" onClick={toggleWeek} className="mt-3">
+              <div className="d-flex align-items-center">
+                {isNextWeek && (
+                  <FontAwesomeIcon
+                    icon={faLongArrowAltLeft}
+                    size="lg"
+                    className="mr-2"
+                  />
+                )}
+                <span>WEEK {isNextWeek ? week - 1 : week + 1}</span>
+                {!isNextWeek && (
+                  <FontAwesomeIcon
+                    icon={faLongArrowAltRight}
+                    size="lg"
+                    className="ml-2"
+                  />
+                )}
+              </div>
+            </Button>
           </Col>
           <Col className="d-flex justify-content-end align-items-center">
             <div className="d-flex flex-column">
-              <Button variant="primary" onClick={refreshData}>
+              <Button variant="secondary" onClick={refreshData}>
                 Refresh
               </Button>
               <span>Requests remaining: {requestsRemaining}</span>
